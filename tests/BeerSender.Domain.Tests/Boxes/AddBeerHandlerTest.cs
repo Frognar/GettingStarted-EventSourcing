a00b7ab252ce536494eb13ed95e56ba3492ffa7f
frognar <sebastian.przyszlak@gmail.com>
2024-12-06 19:07:52 +0100

    Split files

--- tests/BeerSender.Domain.Tests/Boxes/AddBeerHandlerTest.cs
+++ tests/BeerSender.Domain.Tests/Boxes/AddBeerHandlerTest.cs
@@ -1,35 +1,7 @@
-using System.Diagnostics.CodeAnalysis;
 using BeerSender.Domain.Boxes;
 using BeerSender.Domain.Boxes.Commands;
 
 namespace BeerSender.Domain.Tests.Boxes;
-
-[SuppressMessage(
-    "Maintainability",
-    "CA1515:Consider making public types internal",
-    Justification = "Used for unit tests")]
-public abstract class BoxTest<TCommand> : CommandHandlerTest<TCommand>
-{
-    protected Guid Box_Id => _aggregateId;
-    
-    // Events
-    protected BoxCreated Box_created_with_capacity(int capacity)
-        => new(new BoxCapacity(capacity));
-    
-    protected BeerBottleAdded Beer_bottle_added(BeerBottle bottle)
-        => new(bottle);
-    
-    // Test data
-    [SuppressMessage(
-        "Design",
-        "CA1051:Do not declare visible instance fields")]
-    protected BeerBottle carte_blanche = new(
-        "Wolf",
-        "Carte Blanche",
-        0.0,
-        BeerType.Triple
-    );
-}
 
 public class AddBeerHandlerTest : BoxTest<AddBeerBottle>
 {
