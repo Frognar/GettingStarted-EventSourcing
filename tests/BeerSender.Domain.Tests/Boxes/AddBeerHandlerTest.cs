fb3ced83f569a8e5ac9e8c759a65d48a8af8f097
frognar <sebastian.przyszlak@gmail.com>
2024-12-06 19:06:48 +0100

    Making our tests more readable

--- tests/BeerSender.Domain.Tests/Boxes/AddBeerHandlerTest.cs
+++ tests/BeerSender.Domain.Tests/Boxes/AddBeerHandlerTest.cs
@@ -1,9 +1,37 @@
+using System.Diagnostics.CodeAnalysis;
 using BeerSender.Domain.Boxes;
 using BeerSender.Domain.Boxes.Commands;
 
 namespace BeerSender.Domain.Tests.Boxes;
 
-public class AddBeerHandlerTest : CommandHandlerTest<AddBeerBottle>
+[SuppressMessage(
+    "Maintainability",
+    "CA1515:Consider making public types internal",
+    Justification = "Used for unit tests")]
+public abstract class BoxTest<TCommand> : CommandHandlerTest<TCommand>
+{
+    protected Guid Box_Id => _aggregateId;
+    
+    // Events
+    protected BoxCreated Box_created_with_capacity(int capacity)
+        => new(new BoxCapacity(capacity));
+    
+    protected BeerBottleAdded Beer_bottle_added(BeerBottle bottle)
+        => new(bottle);
+    
+    // Test data
+    [SuppressMessage(
+        "Design",
+        "CA1051:Do not declare visible instance fields")]
+    protected BeerBottle carte_blanche = new(
+        "Wolf",
+        "Carte Blanche",
+        0.0,
+        BeerType.Triple
+    );
+}
+
+public class AddBeerHandlerTest : BoxTest<AddBeerBottle>
 {
     protected override CommandHandler<AddBeerBottle> Handler => new AddBeerBottleHandler(eventStore);
 
@@ -11,25 +39,19 @@ public class AddBeerHandlerTest : CommandHandlerTest<AddBeerBottle>
     public void IfBoxIsEmpty_ThenBottleShouldBeAdded()
     {
         Given(
-            new BoxCreated(new BoxCapacity(6))
+            Box_created_with_capacity(6)
         );
         
         When(
-            new AddBeerBottle(_aggregateId, new BeerBottle(
-                "Wolf",
-                "Carte Blanche",
-                0.0,
-                BeerType.Triple
-            ))
+            Add_beer_bottle(carte_blanche)
         );
 
         Then(
-            new BeerBottleAdded(new BeerBottle(
-                "Wolf",
-                "Carte Blanche",
-                0.0,
-                BeerType.Triple
-            ))
+            Beer_bottle_added(carte_blanche)
         );
     }
+    
+    // Commands
+    protected AddBeerBottle Add_beer_bottle(BeerBottle bottle)
+        => new(Box_Id, bottle);
 }
