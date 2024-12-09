2579bb5e9046f94df062049348ab782a0eccb1ac
frognar <sebastian.przyszlak@gmail.com>
2024-12-09 21:14:15 +0100

    Prepare a Web project

---
+++ src/BeerSender.Web/Controllers/BoxController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeerSender.Web.Controllers;
+
+[ApiController]
+[Route("api/command/box")]
+#pragma warning disable CA1515
+public class BoxController : ControllerBase
+#pragma warning restore CA1515
+{
+    
+}
---
+++ src/BeerSender.Web/Program.cs
@@ -0,0 +1,43 @@
+using Scalar.AspNetCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("Beer Sender API")
+            .WithDownloadButton(true)
+            .WithTheme(ScalarTheme.Moon)
+            .WithDefaultHttpClient(ScalarTarget.JavaScript, ScalarClient.JQuery);
+    });
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+    .WithStaticAssets();
+
+app.Run();
