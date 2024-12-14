cbb8048932acfd85b7ec42cd90f36e3fdbd959e8
frognar <sebastian.przyszlak@gmail.com>
2024-12-14 11:20:04 +0100

    Querying aggregate data

--- src/BeerSender.QueryAPI/Controllers/BoxController.cs
+++ src/BeerSender.QueryAPI/Controllers/BoxController.cs
@@ -1,3 +1,4 @@
+using BeerSender.Domain;
 using BeerSender.Domain.Boxes;
 using BeerSender.QueryAPI.Database;
 using Microsoft.AspNetCore.Mvc;
@@ -7,14 +8,18 @@ namespace BeerSender.QueryAPI.Controllers;
 [ApiController]
 [Route("[controller]")]
 #pragma warning disable CA1515
-public class BoxController(BoxQueryRepository repository) : ControllerBase
+public class BoxController(
+    IEventStore eventStore,
+    BoxQueryRepository repository) : ControllerBase
 #pragma warning restore CA1515
 {
     [HttpGet]
     [Route("{id}")]
     public Box GetById([FromRoute]Guid id)
     {
-        throw new NotImplementedException();
+        EventStream<Box> boxStream = new(eventStore, id);
+        Box box = boxStream.GetEntity();
+        return box;
     }
 
     [HttpGet]
